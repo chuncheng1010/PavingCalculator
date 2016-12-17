b10a538034c2c2aacdc4b4e8df0c00234bbf11f0
lichuncheng <lichuncheng@lichunchengs-mac.local>
2016-12-17 00:03:02 -0800

    Initial Commit

---
+++ DeckingCalculatorPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeckingCalculatorPro
+//
+//  Created by lichuncheng on 12/17/16.
+//  Copyright © 2016 lichuncheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeckingCalculatorPro/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DeckingCalculatorPro
+//
+//  Created by lichuncheng on 12/17/16.
+//  Copyright © 2016 lichuncheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
