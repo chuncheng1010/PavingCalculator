03b187baa79a5e8dd62af6c9701d6b98c71d288b
lichuncheng <lichuncheng@lichunchengs-mac.local>
2017-01-09 06:44:28 -0800

    Paving Calculator 2017-12-30

---
+++ PavingCalculatorPro/SettingsMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsMainViewController.h
+//  PavingCalculatorPro
+//
+//  Created by lichuncheng on 12/29/16.
+//  Copyright © 2016 lichuncheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsMainViewController : UIViewController
+
+@end
---
+++ PavingCalculatorPro/SettingsMainViewController.m
@@ -0,0 +1,49 @@
+//
+//  SettingsMainViewController.m
+//  PavingCalculatorPro
+//
+//  Created by lichuncheng on 12/29/16.
+//  Copyright © 2016 lichuncheng. All rights reserved.
+//
+
+#import "SettingsMainViewController.h"
+
+@interface SettingsMainViewController ()
+
+@end
+
+@implementation SettingsMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    if ( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad )
+    {
+        //return YES; /* Device is iPad */
+        //[self.navigationController.navigationBar setBackgroundImage:[[UIImage imageNamed:@"headerimage.png" ] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)] forBarMetrics:UIBarMetricsDefault];
+        //[self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"headerimage.png"] forBarMetrics:UIBarMetricsDefault];
+        self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"headerimage.png"]];
+        
+    }
+    else{
+        self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"headerimage.png"]];
+        //[self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"headerimage.png"] forBarMetrics:UIBarMetricsDefault];
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
